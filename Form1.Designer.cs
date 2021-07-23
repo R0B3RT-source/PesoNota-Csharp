3742fa09d5f5bc60bbd237027b091e5d8463508b
Robert <robert.costa.ribeiro@gmail.com>
2021-07-22 22:05:35 -0300

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace pesoNota
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLabo = new System.Windows.Forms.TextBox();
+            this.txtAva = new System.Windows.Forms.TextBox();
+            this.txtExame = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite a Nota de Laboratório:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite a Nota de Avaliação Semestral:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Digite a Nota do Exame Final:";
+            // 
+            // txtLabo
+            // 
+            this.txtLabo.Location = new System.Drawing.Point(270, 31);
+            this.txtLabo.Name = "txtLabo";
+            this.txtLabo.Size = new System.Drawing.Size(100, 20);
+            this.txtLabo.TabIndex = 3;
+            // 
+            // txtAva
+            // 
+            this.txtAva.Location = new System.Drawing.Point(270, 106);
+            this.txtAva.Name = "txtAva";
+            this.txtAva.Size = new System.Drawing.Size(100, 20);
+            this.txtAva.TabIndex = 4;
+            // 
+            // txtExame
+            // 
+            this.txtExame.Location = new System.Drawing.Point(270, 187);
+            this.txtExame.Name = "txtExame";
+            this.txtExame.Size = new System.Drawing.Size(100, 20);
+            this.txtExame.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(42, 365);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(286, 365);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(308, 415);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(395, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtExame);
+            this.Controls.Add(this.txtAva);
+            this.Controls.Add(this.txtLabo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Nota Final";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLabo;
+        private System.Windows.Forms.TextBox txtAva;
+        private System.Windows.Forms.TextBox txtExame;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pesoNota
+
+{
+    
+    public partial class Form1 : Form
+    {
+        double labo, ava, exame, labo1, ava1, exame1, media;
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtLabo.Text = "";
+            txtAva.Text = "";
+            txtExame.Text = "";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labo = Convert.ToDouble(txtLabo.Text);
+                ava = Convert.ToDouble(txtAva.Text);
+                exame = Convert.ToDouble(txtExame.Text);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Erro na Conversão de Valores", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            labo1 = labo * 2;
+            ava1 = ava * 3;
+            exame1 = exame * 5;
+
+            media = (labo1 + ava1 + exame1) / 10;
+
+            MessageBox.Show("A sua Média final é: " + media);
+
+        }
+    }
+}
